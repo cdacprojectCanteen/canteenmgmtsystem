59fcd3d3d18fa7f6e8d40a5a327a31ff04aaabf6
Shadab Shamsi <shadablaptop@gmail.com>
2018-01-16 23:05:22 +0530

    Added Controller

--- src/main/java/com/canteenmanagement/controllers/CustomerController.java
+++ src/main/java/com/canteenmanagement/controllers/CustomerController.java
@@ -2,8 +2,11 @@ package com.canteenmanagement.controllers;
 
 import java.util.List;
 
+import javax.validation.Constraint;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,11 @@ public class CustomerController {
 	public boolean addCustomer() {
 		customerService.add(new Customer());
 		return false;
+	}
+	
+	@GetMapping("/{custId}")
+	
+	public Customer getCustomer(@PathVariable Integer custId) {
+		return null;
 	}
 }
